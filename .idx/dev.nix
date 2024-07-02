ccb3daf5547ec99cb706b1b65f238dba9fdc392f
c1101081 <c1101081@planet.kanazawa-it.ac.jp>
2024-07-02 13:19:06 +0000

    いったんこれが動くか試す。動けばこれでよい

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -45,7 +45,7 @@
       # Runs when the workspace is (re)started
       onStart = {
         # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        bun_install = "bun install";
       };
     };
   };
--- app/routes/article.$id.tsx
+++ app/routes/article.$id.tsx
@@ -15,36 +15,16 @@ export default function ArticlePage(){
         return (
             <p>Loading...</p>
         )
+    } else {
+        (document.getElementById("article") as HTMLElement).appendChild(
+            document.createRange().createContextualFragment(data.body)
+        )
     }
     return (
         <main className="w-full bg-slate-200/80 text-base-100 p-5">
             <h2 className="text-secondary-content text-xl font-bold">{data.title}</h2>
-            <article className="prose text-secondary-content">
-                {parse(data.body,{
-                    replace(domNode, index) {
-                        if(domNode instanceof Element && domNode.attributes){
-                            if(domNode.tagName === "h3"){
-                                return (
-                                    <h3 /* {...domNode.attributes}  */className="text-secondary-content">
-                                        {domToReact(domNode.childNodes as DOMNode[])}
-                                    </h3>
-                                )
-                            }else if(domNode.tagName === "h2"){
-                                return (
-                                    <h2 /* {...domNode.attributes} */ className="text-secondary-content">
-                                        {domToReact(domNode.childNodes as DOMNode[])}
-                                    </h2>
-                                )
-                            }else if(domNode.tagName === "a"){
-                                return (
-                                    <div /* {...domNode.attributes} */ className="text-secondary-content">
-                                        {domToReact(domNode.childNodes as DOMNode[])}
-                                    </div>
-                                )
-                            }
-                        }
-                    },
-                })}
+            <article id="article" className="prose text-secondary-content">
+                
             </article>
         </main>
     )
